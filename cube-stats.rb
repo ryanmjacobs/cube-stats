b3ef17b8c0e719b10ea427820c14911a937cfa92
Ryan Jacobs <ryan.mjacobs@gmail.com>
2015-02-04 00:14:07 -0800

    Collect data!

--- cube-stats.rb
+++ cube-stats.rb
@@ -3,4 +3,51 @@
 # cube-stats.rb
 ################################################################################
 
+require "json"
 require "rubiks_cube"
+require "./scramble.rb"
+
+# usage help
+if ARGV.length != 1 && ARGV.length != 2 then
+    puts "Usage: cube-stats <iterations> [scramble_moves]"
+    puts
+    puts "Scramble cube for n interations and record the"
+    puts "amount of moves it takes to solve it."
+    exit 1
+end
+
+# number of iterations to do
+iterations = ARGV[0].to_i
+abort "error: iterations must be >= 1" if iterations < 1
+
+# moves to scramble each iteration (default 100)
+scramble_moves = ARGV[1].to_i != 0 ? ARGV[1].to_i : 100
+
+# create a new cube and a data hash
+cube = RubiksCube::Cube.new
+data = {
+    :iterations => iterations,
+    :scramble_moves => scramble_moves,
+    :moves => Array.new
+}
+
+puts "Running #{iterations} iterations with #{scramble_moves} scramble moves per iteration."
+
+# run the iterations
+iterations.times do |i|
+    # Scramble cube with 100 moves
+    seq = Scramble.get_random_moves(scramble_moves)
+    cube.perform! seq
+
+    # How many moves does it take?
+    solution = RubiksCube::TwoCycleSolution.new(cube)
+
+    puts "Trial ##{(i+1).to_s.rjust(3, "0")} - #{solution.length} moves"
+
+    data[:moves] << solution.length
+end
+
+# dump data to JSON
+File.open("cube-stats.json", "w") do |f|
+    f.write JSON.pretty_generate(data) + "\n"
+end
---
+++ scramble.rb
@@ -0,0 +1,60 @@
+################################################################################
+# scramble.rb
+# See http://shuffle.akselipalen.com/static/shuffle/js/shuffle/model.js
+################################################################################
+
+module Scramble
+    extend self
+
+    @move_types = [
+        "F", "F'", "B", "B'", "L", "L'",
+        "R", "R'", "U", "U'", "D", "D'"
+    ]
+
+    # To prevent unnecessary moves, e.g. sequential F and F',
+    # each move has only some possible successors. The easy way
+    # is to use only movements that will permute the previous.
+    @successors = {
+        "F"  => ["L", "L'", "R", "R'", "U", "U'", "D", "D'"],
+        "F'" => ["L", "L'", "R", "R'", "U", "U'", "D", "D'"],
+        "B"  => ["L", "L'", "R", "R'", "U", "U'", "D", "D'"],
+        "B'" => ["L", "L'", "R", "R'", "U", "U'", "D", "D'"],
+        "L"  => ["F", "F'", "B", "B'", "U", "U'", "D", "D'"],
+        "L'" => ["F", "F'", "B", "B'", "U", "U'", "D", "D'"],
+        "R"  => ["F", "F'", "B", "B'", "U", "U'", "D", "D'"],
+        "R'" => ["F", "F'", "B", "B'", "U", "U'", "D", "D'"],
+        "U"  => ["F", "F'", "B", "B'", "L", "L'", "R", "R'"],
+        "U'" => ["F", "F'", "B", "B'", "L", "L'", "R", "R'"],
+        "D"  => ["F", "F'", "B", "B'", "L", "L'", "R", "R'"],
+        "D'" => ["F", "F'", "B", "B'", "L", "L'", "R", "R'"]
+    }
+
+    def random_move
+        mi = rand(0..11)
+        return @move_types[mi]
+    end
+
+    def random_successor(move_type)
+        mi = rand(0..7)
+        return @successors[move_type][mi]
+    end
+
+    def get_random_moves(count)
+        move  = random_move
+        moves = []
+       
+        # Generate the moves
+        count.times do
+            moves << move
+            move = random_successor(move)
+        end
+
+        # Convert to string
+        str = ""
+        moves.each do |move|
+            str << "#{move} "
+        end
+        
+        return str
+    end
+end
