b5555d8612e7abfa25c19dbb254e560ef41161ef
Ryan Jacobs <ryan.mjacobs@gmail.com>
2015-02-04 20:07:09 -0800

    Extrapolate our findings to the mean of 5.66 seconds.

--- plot.r
+++ plot.r
@@ -25,3 +25,24 @@ text(800, y=0.0030, sprintf("std. dev = %0.2f",sd), col="blue")
 #abline(v=c(m+1*sd,m-1*sd),lty=2,lwd=2,col="blue")
 #abline(v=c(m+2*sd,m-2*sd),lty=2,lwd=2,col="blue")
 #abline(v=c(m+3*sd,m-3*sd),lty=2,lwd=2,col="blue")
+
+# SECONDS #
+
+# based on prior world rec. of 5.66 seconds
+d  <- density(data$moves*0.00979747273, adjust=2)
+m  <- mean(data$moves*0.00979747273)
+sd <- sd(data$moves*0.00979747273)
+abline(v=m,lty=2,lwd=2,col="red")
+
+plot(d,
+     main="Time Solve a Rubik's Cube (Two-Cycle Solution)",
+     xlab="Time (seconds)", ylab="Frequency")
+
+# mean
+abline(v=m,lty=2,lwd=2,col="red")
+
+text(8.00, y=0.32, sprintf("    mean = %0.2f",m),  col="blue")
+text(8.01, y=0.30, sprintf("std. dev = %0.2f",sd), col="blue")
+
+# std. dev
+abline(v=c(m+1*sd,m-1*sd),lty=2,lwd=2,col="blue")
